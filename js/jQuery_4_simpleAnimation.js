3ad92d2b48e4bac9c4e7a46a5b62494a51820134
Zia pmr <gernersnc@gmail.com>
2019-05-22 18:59:56 -0400

    new exercise (Animation)

---
+++ js/jQuery_4_simpleAnimation.js
@@ -0,0 +1,17 @@
+//jQuery_4_simpleAnimation
+$(() => {
+  //$(selector).action();
+
+    //NOTE: "()==>" these two together a function.
+
+    $('#clicker').click(() => {
+      //alert('Hi');
+      $('#square').animate({
+          width : '300px'
+
+
+
+      });  //animate
+
+  }); //clicker
+}); //ready
