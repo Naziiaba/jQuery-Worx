826521abedde75ee85b3baa755342b3e176c5983
Zia pmr <gernersnc@gmail.com>
2019-05-22 20:22:26 -0400

    Exercise 5 - css change

--- js/jQuery_5_enlargeFont.js
+++ js/jQuery_5_enlargeFont.js
@@ -1,1 +1,13 @@
 
+//jQuery_5_enlargeFont
+$(() => {
+  //$(selector).action();
+    //alert("hi"); it is just for testing
+
+    const sizeMap = {
+
+      'switcher-small': n => n/1.4,
+      'switcher-large': n => n*1.4,
+      'switcher-default': () => defaultSize
+    };
+});
