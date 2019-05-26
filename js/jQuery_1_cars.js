19161da42078a897b7668e30cff2dce2cd4a426d
Zia pmr <gernersnc@gmail.com>
2019-05-25 21:33:45 -0400

    some linking updates. a new image added.

--- js/jQuery_1_cars.js
+++ js/jQuery_1_cars.js
@@ -13,31 +13,31 @@ $(() => {
     switch (favoriteCar) {
       case 'BMW':
       alert("BMW");
-          $("#theCar").html("<img src='images/lexus.jpg' width='300' height='200'"+ "\">");
+          $("#theCar").html("<img src='/images/lexus.jpg' width='320' height='260' "+ "\">");
           break;
 
       case 'BMW':
-          $("#theCar").html("<img src='images/bmw.jpg' width='300' height='200'"+ "\">");
+          $("#theCar").html("<img src='/images/bmw.jpg' width='320' height='260' "+ "\">");
           break;
 
       case 'Tesla':
-          $("#theCar").html("<img src='images/tesla.jpg' width='300' height='200'"+ "\">");
+          $("#theCar").html("<img src='/images/tesla.jpg' width='320' height='260' "+ "\">");
           break;
 
       case 'Ford':
-          $("#theCar").html("<img src='images/ford.jpg' width='300' height='200'"+ "\">");
+          $("#theCar").html("<img src='/images/ford.jpg' width='320' height='260' "+ "\">");
           break;
 
       case 'Toyota':
-          $("#theCar").html("<img src='images/toyota.jpg' width='300' height='200'"+ "\">");
+          $("#theCar").html("<img src='/images/toyota.jpg' width='320' height='260' "+ "\">");
           break;
 
       case 'Bentley':
-          $("#theCar").html("<img src='images/bentley.jpg' width='300' height='200'"+ "\">");
+          $("#theCar").html("<img src='/images/bentley.jpg' width='320' height='260' "+ "\">");
           break;
 
       case 'Jeep':
-          $("#theCar").html("<img src='images/jeep.png' width='300' height='200'"+ "\">");
+          $("#theCar").html("<img src='/images/jeep.png' width='320' height='260' "+ "\">");
           break;
     } //switch
 
