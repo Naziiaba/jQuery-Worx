1014fdbce547c48546f46ab637d1a6d939ac4932
Zia pmr <gernersnc@gmail.com>
2019-06-03 20:59:27 -0400

    Update jQuery_9_SearchPrice.js

--- js/jQuery_9_SearchPrice.js
+++ js/jQuery_9_SearchPrice.js
@@ -1,5 +1,24 @@
 //alert('Hello')
 $(() => {
-  //$(selector).action;
+  //$(selector).actions;
 $('input[name=search]')
-})
+.click((e) => {
+    $('table tr').show();
+    const maxPrice = Number.parseFloat($('input[name=price]').val());
+    //alert(maxPrice)
+    if (Number.isNaN(maxPrice)) {
+      return;
+    }
+    $('table')
+    .find('tr:has(td)')              //.(dot) means action
+    .not((i, element)  => {
+        const rowPrice = Number.parseFloat($(element)
+        .children(':nth-child(5)')
+        .text()
+      ); //rowPricprice
+      //alert(rowPrice);
+      return rowPrice <= maxPrice;  // <= less than or equal
+    })
+    .hide();  //not
+}); //click
+}); //ready
