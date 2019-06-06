cfbb81443f18f29a944d9b49b79c91593a1c70f0
Zia pmr <gernersnc@gmail.com>
2019-06-05 21:06:58 -0400

    Update jQuery_11_DataGridFilter.js

--- js/jQuery_11_DataGridFilter.js
+++ js/jQuery_11_DataGridFilter.js
@@ -5,9 +5,18 @@ $(() => {
 const stripe = () => {
   $('#news')
   .find('tr.alt')
-
-
+  .removeClass('alt')
+  .end()
+  .find('tbody')
+  .each((i, element) => {
+    $(element)
+    .children('visible')
+    .has('td')
+    .filter(i => (i%4)<2)
+    .addClass('alt')
+  }); //each
 } //stripe
 
+  stripe();
 
   }); //ready
