a88068ccd5b000e8960c9625301ce8eefa102e90
Zia pmr <gernersnc@gmail.com>
2019-06-06 09:12:33 -0400

    Update jQuery_10_SearchPriceQty.js

--- js/jQuery_10_SearchPriceQty.js
+++ js/jQuery_10_SearchPriceQty.js
@@ -21,4 +21,28 @@ $('input[name=search]')
     })
     .hide();  //not
 }); //click
+
+//qty search
+$('input[name=searchQTY]')
+.click((e) => {
+  //alert("Hello");
+  $('table tr').show();
+  const maxQTY = Number.parseFloat($('input(name=qty)').val());
+  //alert(maxQTY)
+  if (Number.isNaN(maxQTY)) {
+    return;
+  }
+  $('table')
+  .find('tr:has(td)')
+  .not((i, element) => {
+    const rowQTY = Number.parseFloat($(element)
+    .children(':nth-child(4)')
+  .text()
+); //rowQTY
+return rowQTY <= maxQTY;
+  })
+  .hide();
+}); //click
 }); //ready
+
+//alert('$' + parseFloat(.text(), 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g,"$1,").toString())
