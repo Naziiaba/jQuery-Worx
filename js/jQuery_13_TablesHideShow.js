3528b1afee013476f5c6099b5dee5f8f777671b8
Zia pmr <gernersnc@gmail.com>
2019-06-10 20:23:57 -0400

    Update jQuery_13_TablesHideShow.js

--- js/jQuery_13_TablesHideShow.js
+++ js/jQuery_13_TablesHideShow.js
@@ -8,10 +8,15 @@ $('table')
     const tableId = "table-title-" + i;
     const tableText = 'Table ' + (i+1);
     $('<h3/>', {
-
+      'class': 'table-title',
+      'id':tableId,
+      'text':tableText,
+      'data':{index: i},
+      'click': (e) => {
+        e.preventDefault();
+        $(table).fadeToggle();
+      },
+      'css':{color:'blue', cursor:'pointer'}
     }).insertBefore(table);  //h3
-
-
-
-  )}; //each
+  }); //each
 }); //ready
